805eae0fe8c6ac99d7d97ed574b1731d8d416881
Chih-hao Su <nikob@live.cn>
2024-07-13 22:07:18 +0800

    Delete unnecessary file

--- developerLocalConfig.gradle.kts
+++
@@ -1,5 +0,0 @@
-ext {
-    test_server_path = "/run/media/chihhao-su/PROJECT/GAME/NPCCRAFT/sponge_test_server/"
-    test_server_plugin_path = "/run/media/chihhao-su/PROJECT/GAME/NPCCRAFT/sponge_test_server/mods/plugins"
-    test_server_jar = "/run/media/chihhao-su/PROJECT/GAME/NPCCRAFT/sponge_test_server/forge-1.12.2-14.23.5.2860.jar"
-}
